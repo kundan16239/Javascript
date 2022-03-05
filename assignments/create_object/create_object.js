d3524b506c8d9988800008a6d89f6baed560d9c6
unknown <kundan16239@gmail.com>
2022-03-05 18:14:29 +0530

    message

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
-    // Write your code here
+    var Object = {};
+    var arr;
+    for (var i = 0; i < arr.length; i += 2) {
+        Object[arr[i]] = arr[i + 1]
+    }
+    return Object
 }
 
 module.exports = CreateObject;
